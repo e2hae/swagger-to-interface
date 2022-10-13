e6891906fe5962de3bac0414ce417b827f51de06
jae hyun <1994dbwogus@gmail.com>
2022-10-13 18:28:00 +0900

    feat: 소스 수정

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import { generate } from 'swagger-typescript';
 import path from 'path';
 import url from 'url';
 
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 const swaggerOption = {
   language: 'typescript',
 };
@@ -15,7 +13,7 @@ commander
   .arguments('<url>')
   .arguments('<dir>')
   .action((url, dir) => {
-    return generate({ ...swaggerOption, url, dir: path.join(__dirname, dir) })
+    return generate({ ...swaggerOption, url, dir: path.join(process.cwd(), dir) })
       .then(() => console.log('Success Swagger to Typescript Interface'))
       .catch(console.error);
   })
